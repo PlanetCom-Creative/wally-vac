b301a14f1a97c06f8c1133fe4a4ed0c7d24d6a6b
Jeffrey A <44594550+jeffrey-agyepong@users.noreply.github.com>
2024-08-14 16:57:26 -0600

    Update stackbit.config.js

--- stackbit.config.js
+++ stackbit.config.js
@@ -2,29 +2,10 @@ import { defineStackbitConfig } from "@stackbit/types";
 import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
-  ssgName: 'custom',
+  ssgName: 'astro',
   stackbitVersion: "~0.6.0",
     nodeVersion: '16',
-  contentSources: [
-    new GitContentSource({
-      rootPath: __dirname,
-      contentDirs: ["content"],
-      models: [
-        {
-          name: "Page",
-          type: "page",
-          urlPath: "/{slug}",
-          filePath: "content/pages/{slug}.json",
-          fields: [{ name: "title", type: "string", required: true }]
-        }
-      ],
-      assetsConfig: {
-        referenceType: "relative",
-        staticDir: "public",
-        uploadDir: "images",
-        publicPath: "/"
-      }
-    })
-  ]
+    contentSources: [ /* ... */ ]
+    modelExtensions: [{ name: 'page', type: 'page', urlPath: '/{slug}' }],
 });
 
