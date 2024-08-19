46e80782c4b2f835347805189f31a43c28568d54
Jeffrey A <44594550+jeffrey-agyepong@users.noreply.github.com>
2024-08-19 12:03:38 -0600

    contact

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output:'static',
+  site:'https://walyvac.ca',
   integrations: [tailwind()],
   server: {
     port: 3000, // Set the port to 3000
